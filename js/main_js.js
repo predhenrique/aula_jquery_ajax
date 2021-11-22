39dfa5c15c46e7d0b07c8d89c574f193b268cb5a
predhenrique <pedro.he97@gmail.com>
2021-11-22 16:15:41 -0300

    Aula sobre Jquery e javax

---
+++ js/main_js.js
@@ -0,0 +1,15 @@
+function consultaCep(){
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json";
+
+    console.log(cep);
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            $("#logradouro").html(response.logradouro);
+            document.getElementById("logradouro").innerHTML = response.logradouro;
+        }
+    })
+}
